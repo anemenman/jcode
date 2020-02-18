2f30550a494ebfac1efc4bb16ffd5eb85f1680b2
A A <aa@MacBook-Pro-A.local>
2020-02-18 17:34:20 +0200

    Autoboxing

---
+++ src/main/java/jcode/ch_01_java_core/q_26_autoboxing/Autoboxing_01.java
@@ -0,0 +1,14 @@
+package jcode.ch_01_java_core.q_26_autoboxing;
+
+public class Autoboxing_01 {
+
+  public static void main(String[] args) {
+
+    //Autoboxing
+    Character c = 'a';
+
+    //Unboxing
+    int i = new Integer(10);
+  }
+
+}
