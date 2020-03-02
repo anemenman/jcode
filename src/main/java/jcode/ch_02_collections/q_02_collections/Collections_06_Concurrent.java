f38154cf55bdbfdc2f68745b94b5b0f17444becb
A A <aa@MacBook-Pro-A.local>
2020-03-02 21:23:34 +0200

    Collections concurrent

---
+++ src/main/java/jcode/ch_02_collections/q_02_collections/Collections_06_Concurrent.java
@@ -0,0 +1,15 @@
+package jcode.ch_02_collections.q_02_collections;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class Collections_06_Concurrent {
+
+  public static void main(String[] args) {
+    ConcurrentHashMap concurrentHashMap;
+    CopyOnWriteArrayList copyOnWriteArrayList;
+    CopyOnWriteArraySet copyOnWriteArraySet;
+  }
+
+}
