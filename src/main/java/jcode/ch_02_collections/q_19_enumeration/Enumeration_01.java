fec9077b850c3e36566dc41f2e7c71f6fa79d37c
A A <aa@MacBook-Pro-A.local>
2020-03-03 15:31:06 +0200

    Enumeration

---
+++ src/main/java/jcode/ch_02_collections/q_19_enumeration/Enumeration_01.java
@@ -0,0 +1,18 @@
+package jcode.ch_02_collections.q_19_enumeration;
+
+import java.util.Enumeration;
+
+public class Enumeration_01 {
+
+  public static void main(String[] args) {
+    /**
+     * public interface Enumeration<E> {
+     *
+     *  boolean hasMoreElements();
+     *  E nextElement();
+     * }
+     */
+    Enumeration<String> e;
+  }
+
+}
