8f0fe703e7ab176ddf3fbdb322836d3d096220c5
A A <aa@MacBook-Pro-A.local>
2020-03-16 22:03:19 +0200

    WeakHashMap

---
+++ src/main/java/jcode/ch_02_collections/q_61_weakhashmap/WeakHashMap_01.java
@@ -0,0 +1,19 @@
+package jcode.ch_02_collections.q_61_weakhashmap;
+
+import java.lang.ref.SoftReference;
+import java.util.WeakHashMap;
+
+public class WeakHashMap_01 {
+
+  public static void main(String[] args) {
+
+    /**
+     * public class WeakHashMap<K,V> extends AbstractMap<K,V> implements Map<K,V> {
+     *
+     * 
+     */
+    WeakHashMap<String, String> weakHashMap;
+
+  }
+
+}
