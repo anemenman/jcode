dcc1d1fa686fc59cd9c2b6d8a01db210b7ff74fd
A A <aa@MacBook-Pro-A.local>
2020-03-17 22:20:25 +0200

    Collections

---
+++ src/main/java/jcode/ch_02_collections/q_70_java_util_collections/Collections_01.java
@@ -0,0 +1,32 @@
+package jcode.ch_02_collections.q_70_java_util_collections;
+
+import java.util.Collections;
+
+public class Collections_01 {
+
+  public static void main(String[] args) {
+
+    /**
+     *  java.util.Collections
+     *
+     *  ...
+     *  void sort
+     *  boolean addAll
+     *  void copy
+     *  emptyList()
+     *  emptyMap()
+     *  T max
+     *  T min
+     *  void reverse
+     *  List<T> singletonList(T o)
+     *  Map<K,V> singletonMap
+     *  List<T> synchronizedList
+     *  Map<K,V> synchronizedMap
+     *  List<T> unmodifiableList
+     *  Map<K,V> unmodifiableMap
+     *  ...
+     */
+    Collections c;
+  }
+
+}
