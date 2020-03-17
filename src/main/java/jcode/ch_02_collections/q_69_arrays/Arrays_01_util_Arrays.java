32bc68b753a26846637e1e7b47f5e64f239b242b
A A <aa@MacBook-Pro-A.local>
2020-03-17 22:09:18 +0200

    Arrays

---
+++ src/main/java/jcode/ch_02_collections/q_69_arrays/Arrays_01_util_Arrays.java
@@ -0,0 +1,24 @@
+package jcode.ch_02_collections.q_69_arrays;
+
+import java.util.Arrays;
+
+public class Arrays_01_util_Arrays {
+
+  public static void main(String[] args) {
+
+    /**
+     * ...
+     * public static int binarySearch
+     * public static boolean[] copyOf
+     * public static boolean[] copyOfRange
+     * public static void fill
+     * public static void parallelSort
+     * public static void sort
+     * public static Spliterator.OfDouble spliterator
+     * public static IntStream stream
+     * ...
+     */
+    Arrays a;
+  }
+
+}
---
+++ src/main/java/jcode/ch_02_collections/q_69_arrays/Arrays_02_lang_reflect_Array.java
@@ -0,0 +1,14 @@
+package jcode.ch_02_collections.q_69_arrays;
+
+import java.lang.reflect.Array;
+
+public class Arrays_02_lang_reflect_Array {
+
+  public static void main(String[] args) {
+    /**
+     *  package java.lang.reflect;
+     *  public final class Array {
+     */
+    Array a;
+  }
+}
