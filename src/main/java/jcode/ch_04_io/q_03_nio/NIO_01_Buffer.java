5c4d31679f2757f507120d298ea353a84f9aa51e
A A <aa@MacBook-Pro-A.local>
2020-04-07 15:55:37 +0300

    NIO buffer, charset

---
+++ src/main/java/jcode/ch_04_io/q_03_nio/NIO_01_Buffer.java
@@ -0,0 +1,22 @@
+package jcode.ch_04_io.q_03_nio;
+
+import java.nio.Buffer;
+
+public class NIO_01_Buffer {
+
+  public static void main(String[] args) {
+
+    /**
+     * public abstract class Buffer {
+     *
+     * public final Buffer mark()
+     * public final Buffer reset()
+     * public final Buffer clear()
+     * public final Buffer flip()
+     * ...
+     *
+     */
+    Buffer buffer;
+  }
+
+}
---
+++ src/main/java/jcode/ch_04_io/q_03_nio/NIO_02_Charset.java
@@ -0,0 +1,19 @@
+package jcode.ch_04_io.q_03_nio;
+
+import java.nio.charset.Charset;
+
+public class NIO_02_Charset {
+
+  public static void main(String[] args) {
+    /**
+     * public abstract class Charset implements Comparable<Charset>
+     *
+     * public abstract CharsetDecoder newDecoder()
+     * public abstract CharsetEncoder newEncoder()
+     * ...
+     *
+     */
+    Charset charset;
+  }
+
+}
