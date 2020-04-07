058162cdda966924676446c38766aa27d8c4ba03
A A <aa@MacBook-Pro-A.local>
2020-04-07 18:40:13 +0300

    RandomAccessFile

---
+++ src/main/java/jcode/ch_04_io/q_08_random_access_file/RandomAccessFile_01.java
@@ -0,0 +1,15 @@
+package jcode.ch_04_io.q_08_random_access_file;
+
+import java.io.RandomAccessFile;
+
+public class RandomAccessFile_01 {
+
+  public static void main(String[] args) {
+
+    /**
+     * public class RandomAccessFile implements DataOutput, DataInput, Closeable {
+     */
+    RandomAccessFile randomAccessFile;
+  }
+
+}
