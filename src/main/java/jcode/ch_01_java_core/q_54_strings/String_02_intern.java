3ece5c9d980b7177a6419ece634f089fd7401e44
A A <aa@MacBook-Pro-A.local>
2022-02-01 21:16:25 +0200

    Str 2

--- src/main/java/jcode/ch_01_java_core/q_54_strings/String_02_intern.java
+++ src/main/java/jcode/ch_01_java_core/q_54_strings/String_02_intern.java
@@ -2,15 +2,14 @@ package jcode.ch_01_java_core.q_54_strings;
 
 public class String_02_intern {
 
-  public static void main(String[] args) {
-    String s = "String";
-    System.out.println(s);
-    System.out.println(s.intern());//Get string from string pull
-    String s2 = "String2";
-    String s3 = "String";
-    System.out.println(s == s3);
-    System.out.println(s.intern() == s2.intern());
-    System.out.println(s.intern() == s3.intern());
-  }
-
+    public static void main(String[] args) {
+        String s = "String";
+        System.out.println(s);
+        System.out.println("s.intern()--->" + s.intern());//Get string from string pull
+        String s2 = "String2";
+        String s3 = "String";
+        System.out.println(s == s3);
+        System.out.println(s.intern() == s2.intern());
+        System.out.println(s.intern() == s3.intern());
+    }
 }
