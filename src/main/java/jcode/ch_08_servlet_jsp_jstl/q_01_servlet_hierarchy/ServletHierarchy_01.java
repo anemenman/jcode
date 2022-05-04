22cf1f28a7a6691e28464a2a796fd2b7cb02501f
Andrey <akonovalov314@gmail.com>
2022-05-04 23:11:34 +0300

    Servlet hierarchy

---
+++ src/main/java/jcode/ch_08_servlet_jsp_jstl/q_01_servlet_hierarchy/ServletHierarchy_01.java
@@ -0,0 +1,32 @@
+package jcode.ch_08_servlet_jsp_jstl.q_01_servlet_hierarchy;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletHierarchy_01 {
+    
+    public static void main(String[] args) {
+
+        Servlet servlet;
+
+        GenericServlet genericServlet;
+
+        ServletConfig servletConfig;
+
+        HttpServlet httpServlet;
+
+        ServletRequest servletRequest;
+
+        HttpServletRequest httpServletRequest;
+
+        ServletContext servletContext;
+
+        RequestDispatcher requestDispatcher;
+
+        ServletResponse servletResponse;
+
+        HttpServletResponse httpServletResponse;
+    }
+}
